0ff8730cfa35a079c9ff72fb8008386735981f85
Manoel Monteiro <manoel.monteiro1@outlook.com>
2023-04-06 06:54:08 -0300

    created ex013

--- aula13/aula13.js
+++ aula13/aula13.js
@@ -2,6 +2,7 @@
 let umaString = 'Um "texto"'; //Assim irá utilizar as aspas de dentro da string normalmente.
 let umaString2 = "Um 'texto'"; //Assim irá utilizar as aspas de dentro da string normalmente.
 let umaString3 = 'Um \"texto\"'; //Assim irá utilizar as aspas de dentro da string normalmente.
+let umaString4 = "O rato roeu a roupa do rei de Roma."
 
 console.log(umaString.charAt(5));
 console.log(umaString[3]);
@@ -14,4 +15,14 @@ console.log(umaString2.indexOf("U", 2)); //Procura no índice da String começan
 
 console.log(umaString3.match(/[a-z]/g)); //Procura na string todas as letras minúsculas. Isso é uma expressão regular.
 console.log(umaString.search(/x/)); // Procura a letra da expressão em específico na string.
+
 console.log(umaString.replace('Um', 'Outra')); //Substitui a palavra 'Um' por 'Outra'.
+console.log(umaString4.replace(/r/g, '#')); //Substitui todas as letras R por #, utilizando expressão regular
+
+console.log(umaString4.length); //Retorna o tamanho em caracteres da String
+console.log(umaString4.slice(-5, -1)); //Corta um intervalo da String, levando em consideração o índice da string. Também aceita valores negativos.
+
+console.log(umaString4.split(' ')); //Divide as palavras da string em um array
+
+console.log(umaString4.toUpperCase()); //Passa a string inteira para maiúscula
+console.log(umaString4.toLowerCase()); //Passa a string inteira para minúscula
